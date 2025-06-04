839a7bf70105b4511e1b2e41aa29aff34c865f74
Dant3 Leal <danleal2023@udec.cl>
2025-06-04 17:32:07 -0400

    Añadir metodos a Paneles gráficos
    Se añadió el método constructor y el método paintComponent() a PanelComprador.java y PanelExpendedor.java en el apartado gráfico.

--- src/grafica/PanelComprador.java
+++ src/grafica/PanelComprador.java
@@ -1,3 +1,11 @@
+import java.awt.*;
+
 public class PanelComprador {
-    
+    // Metodo constructor
+    public PanelComprador() {
+    }
+
+    public void paintComponent(Graphics g) {
+
+    }
 }
--- src/grafica/PanelExpendedor.java
+++ src/grafica/PanelExpendedor.java
@@ -1,3 +1,11 @@
+import java.awt.*;
+
 public class PanelExpendedor {
-    
+    // Metodo constructor
+    public PanelExpendedor() {
+    }
+
+    public void paintComponent(Graphics g) {
+
+    }
 }
