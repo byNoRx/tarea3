84bb556619782a383bd712599e2456b6f1a1af46
Dant3 Leal <danleal2023@udec.cl>
2025-06-04 17:40:13 -0400

    Fix PanelPrincipal
    - Se arregla un error de tipeo.
    - Se importa la dependencia faltante (java.awt.*) para que funcione correctamente.

--- src/grafica/PanelPrincipal.java
+++ src/grafica/PanelPrincipal.java
@@ -1,12 +1,13 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class PanelPrincipal extends JPanel {//se ve en el centro de la ventana
     private PanelComprador com;
     private PanelExpendedor exp;
 
     public PanelPrincipal () {
+        com = new PanelComprador();
         exp = new PanelExpendedor ();
-        cf = new PanelComprador();
         this.setBackground(Color.white);
     }
 
