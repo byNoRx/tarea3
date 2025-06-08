e858d8840cab9838e0cdc7120edcc50566397d3d
byNoRx <norxgamer12@gmail.com>
2025-06-07 23:35:28 -0400

    Comentar temporalmente código relacionado a comprador para enfocarse en expendedor y mejorar claridad de comentarios

--- src/grafica/PanelPrincipal.java
+++ src/grafica/PanelPrincipal.java
@@ -6,15 +6,16 @@ public class PanelPrincipal extends JPanel {//se ve en el centro de la ventana
     private PanelExpendedor exp;
 
     public PanelPrincipal () {
-        com = new PanelComprador();
-        exp = new PanelExpendedor ();
         this.setBackground(Color.white);
+        //com = new PanelComprador(0,0,new Comprador());
+        exp = new PanelExpendedor(0, 0, new Expendedor(5));
     }
 
-    public void paintComponent (Graphics g) { //todo se dibuja a partir de este método
-        super.paintComponent(g); //llama al método paint al que hace override en la super clase
-        //el de la super clase solo pinta el fondo (background)
-        com.paintComponent(g); //llama al metodo paintComponent definido en el PanelComprador
-        exp.paintComponent(g); //llama al metodo paintComponent definido en el PanelExpendedor
+    @Override
+    public void paintComponent (Graphics g) {   // Todo se dibuja a partir de este método
+        super.paintComponent(g);                // Llama al método paint al que hace override en la super clase
+                                                // El de la super clase solo pinta el fondo (background)
+        //com.paintComponent(g);                  // Llama al metodo paintComponent definido en el PanelComprador
+        exp.paintComponent(g);                  // Llama al metodo paintComponent definido en el PanelExpendedor
     }
 }
