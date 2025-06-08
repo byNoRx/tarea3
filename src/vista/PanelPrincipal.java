8d5cfa3428666fcd968f54fec12bf74ca78e58d4
byNoRx <norxgamer12@gmail.com>
2025-06-07 23:40:57 -0400

    Renombrar carpeta de logica a modelo, y de grafica a vista para mayor claridad

--- src/vista/PanelPrincipal.java
+++ src/vista/PanelPrincipal.java
@@ -8,7 +8,7 @@ public class PanelPrincipal extends JPanel {//se ve en el centro de la ventana
     public PanelPrincipal () {
         this.setBackground(Color.white);
         //com = new PanelComprador(0,0,new Comprador());
-        exp = new PanelExpendedor(0, 0, new Expendedor(5));
+        exp = new PanelExpendedor(100, 100, new Expendedor(5));
     }
 
     @Override
