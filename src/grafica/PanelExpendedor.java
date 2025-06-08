e2cd5d8a7b26154289447d40f871f8f406458bbd
byNoRx <norxgamer12@gmail.com>
2025-06-07 23:05:24 -0400

    Añadir variables de posición y expendedor que se inicializan en el constructor de PanelExpendedor y dibujar

--- src/grafica/PanelExpendedor.java
+++ src/grafica/PanelExpendedor.java
@@ -1,11 +1,20 @@
 import java.awt.*;
 
 public class PanelExpendedor {
+    private int posX;
+    private int posY;
+    private Expendedor expendedor;
+
     // Metodo constructor
-    public PanelExpendedor() {
+    public PanelExpendedor(int posX, int posY, Expendedor expendedor) {
+        this.posX = posX;
+        this.posY = posY;
+        this.expendedor = expendedor;
     }
 
+    @Override
     public void paintComponent(Graphics g) {
-
+        g.setColor(Color.black);
+        g.fillRect(posX, posY, ObjetoSize.EXP.getWidth(), ObjetoSize.EXP.getHeight());
     }
 }
