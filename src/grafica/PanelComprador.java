751a6d82238ded68977f645df40770671f5ab4a3
byNoRx <norxgamer12@gmail.com>
2025-06-07 23:04:36 -0400

    Añadir variables de posición y comprador que se inicializan en el constructor de PanelComprador y dibujar

--- src/grafica/PanelComprador.java
+++ src/grafica/PanelComprador.java
@@ -1,11 +1,26 @@
 import java.awt.*;
 
 public class PanelComprador {
-    // Metodo constructor
-    public PanelComprador() {
+    private int posX;
+    private int posY;
+    private Comprador comprador;
+
+    /**
+     * Constructor
+     * 
+     * @param posX
+     * @param posY
+     * @param comprador
+     */
+    public PanelComprador(int posX, int posY, Comprador comprador) {
+        this.posX = posX;
+        this.posY = posY;
+        this.comprador = comprador;
     }
 
+    @Override
     public void paintComponent(Graphics g) {
-
+        g.setColor(Color.black);
+        g.fillRect(posX, posY, ObjetoSize.COM.getWidth(), ObjetoSize.COM.getHeight());
     }
 }
