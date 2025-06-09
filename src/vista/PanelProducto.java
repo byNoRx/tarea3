5e2c7177f6754aadf17d7569db4c52185cc66448
byNoRx <norxgamer12@gmail.com>
2025-06-09 05:01:12 -0400

    Añadir coordenadas a producto

--- src/vista/PanelProducto.java
+++ src/vista/PanelProducto.java
@@ -1,7 +1,24 @@
+import javax.swing.*;
+import java.awt.*;
 
+public class PanelProducto extends JPanel {
+    private int posX;
+    private int posY;
+    //private Producto producto;
 
-public class PanelProducto  {
+    public PanelProducto(int posX, int posY, Producto producto) {
+        this.posX = posX;
+        this.posY = posY;
+        //.producto = producto;
+    }
 
+    /*@Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        g.setColor(Color.green);
+        g.fillRect(posX, posY, ObjetoSize.PRO.getWidth(), ObjetoSize.PRO.getHeight());
+    }*/
 }
 
 
