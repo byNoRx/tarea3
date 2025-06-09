6885722efdf7801f51568a4255eaba16f57ca364
byNoRx <norxgamer12@gmail.com>
2025-06-09 05:00:07 -0400

    Comentar código de menú por no uso y cambiar nombre de variable para mayor comprensión

--- src/vista/Ventana.java
+++ src/vista/Ventana.java
@@ -2,22 +2,19 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Ventana extends JFrame {
-    private PanelPrincipal dp; // Un panel para objetos gráficos
-    Ventana(){
+    private PanelPrincipal panelPrincipal; // Un panel para objetos gráficos
+
+    Ventana() {
         super(); // Hereda metodos de JFrame
 
-        dp=new PanelPrincipal();
-        dp.setLayout(new BorderLayout());
-        Menu menu = new Menu();
-        dp.add(menu,BorderLayout.EAST);
+        panelPrincipal = new PanelPrincipal();
+        panelPrincipal.setLayout(new BorderLayout());
+        //Menu menu = new Menu();
+        //panelPrincipal.add(menu,BorderLayout.EAST);
 
-        this.add(dp,BorderLayout.CENTER); // Se agrega al centro
+        this.add(panelPrincipal,BorderLayout.CENTER); // Se agrega al centro
         this.setDefaultCloseOperation(EXIT_ON_CLOSE); // Cerrar aplicación
         this.setSize(600,800);
         this.setVisible(true);
-
-
-
-
-    }
+    } 
 }
