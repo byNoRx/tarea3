09a1b09b9ed53b3f9c5d303f965de9d9529dab9b
byNoRx <norxgamer12@gmail.com>
2025-06-09 08:01:36 -0400

    Añadir switch para cambiar colores, añadir línea para inicializar producto

--- src/vista/PanelProducto.java
+++ src/vista/PanelProducto.java
@@ -4,19 +4,27 @@ import java.awt.*;
 public class PanelProducto extends JPanel {
     private int posX;
     private int posY;
-    //private Producto producto;
+    private Producto producto;
 
     public PanelProducto(int posX, int posY, Producto producto) {
         this.posX = posX;
         this.posY = posY;
-        //.producto = producto;
+        this.producto = producto;
     }
 
     @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
 
-        g.setColor(Color.red);
+        switch (producto) {
+            case CocaCola cocaCola -> g.setColor(Color.red);
+            case Fanta fanta -> g.setColor(Color.orange);
+            case Sprite sprite -> g.setColor(Color.green);
+            case Snickers snickers -> g.setColor(Color.yellow);
+            case Super8 super8 -> g.setColor(Color.black);
+            case null, default -> g.setColor(Color.white);
+        }
+
         g.fillRect(posX, posY, ObjetoSize.OBJ.getWidth(), ObjetoSize.OBJ.getHeight());
     }
 }
