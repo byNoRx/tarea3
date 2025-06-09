da637e6f7c138aa753a07ba29153b33f26190645
byNoRx <norxgamer12@gmail.com>
2025-06-09 08:04:21 -0400

    Hacer que en caso de que el producto sea inválido, no dibujar

--- src/vista/PanelProducto.java
+++ src/vista/PanelProducto.java
@@ -16,13 +16,17 @@ public class PanelProducto extends JPanel {
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
 
+        if (producto == null) {
+            return;
+        }
+
         switch (producto) {
             case CocaCola cocaCola -> g.setColor(Color.red);
             case Fanta fanta -> g.setColor(Color.orange);
             case Sprite sprite -> g.setColor(Color.green);
             case Snickers snickers -> g.setColor(Color.yellow);
             case Super8 super8 -> g.setColor(Color.black);
-            case null, default -> g.setColor(Color.white);
+            default -> g.setColor(Color.white);
         }
 
         g.fillRect(posX, posY, ObjetoSize.OBJ.getWidth(), ObjetoSize.OBJ.getHeight());
