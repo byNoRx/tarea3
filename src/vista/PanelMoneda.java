9a53d60f6613a4d016c924af13e76102fd99d392
byNoRx <norxgamer12@gmail.com>
2025-06-09 06:09:11 -0400

    Añadir coordenadas y moneda en el constructor para que se pueda dibujar

--- src/vista/PanelMoneda.java
+++ src/vista/PanelMoneda.java
@@ -1,3 +1,28 @@
-public class PanelMoneda {
-    
+import javax.swing.*;
+import java.awt.*;
+
+public class PanelMoneda extends JPanel {
+    private int posX;
+    private int posY;
+    private Moneda moneda;
+
+    PanelMoneda(int posX, int posY, Moneda moneda) {
+        this.posX = posX;
+        this.posY = posY;
+
+        this.moneda = moneda;
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        g.setColor(Color.yellow);
+        g.fillOval(posX, posY, ObjetoSize.OBJ.getWidth(), ObjetoSize.OBJ.getHeight());
+
+        /*if (moneda.getValor() == 100) {
+            g.setColor(Color.lightGray);
+        }*/
+
+    }
 }
