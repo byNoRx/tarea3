5c5b724b98d863f0f73e4c75acfe42333eb9b7c5
byNoRx <norxgamer12@gmail.com>
2025-06-09 16:33:26 -0400

    Hacer que al cliquear el expendedor, este sea rellenado

--- src/vista/PanelPrincipal.java
+++ src/vista/PanelPrincipal.java
@@ -1,9 +1,13 @@
 import javax.swing.*;
+
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
-public class PanelPrincipal extends JPanel implements ActionListener {    // Se ve en el centro de la ventana
+public class PanelPrincipal extends JPanel implements ActionListener, MouseListener {    // Se ve en el centro de la ventana
     // Comprador  y expendedor
     private Comprador modeloComprador;
     private Expendedor modeloExpendedor;
@@ -14,36 +18,32 @@ public class PanelPrincipal extends JPanel implements ActionListener {    // Se 
     private PanelComprador com;
     private PanelExpendedor exp;
 
+    int numProductos;
+
     // Botones para el menú interactivo
     private JPanel BigPanel, panel1, panel2,panel3;
     private JButton boton1, boton2, boton3, boton4, boton5, boton6,boton7,boton8,boton9,boton10,boton11,boton12,boton13,boton14,boton15;
 
     public PanelPrincipal() {
-        modeloExpendedor = new Expendedor(5);
+
+        numProductos = 5;
+
+        modeloExpendedor = new Expendedor(numProductos);
 
         // Personalizar panel principal
         setLayout(new BorderLayout());
         setBackground(Color.white);
+        addMouseListener(this);
 
-        // Crear subpaneles
-        // Izquierda
-        JPanel left = new JPanel();
-        add(left, BorderLayout.CENTER);
-        left.setBackground(Color.WHITE);
-        // Derecha
-        JPanel right = new JPanel();
-        right.setLayout(new BorderLayout());
-        right.setPreferredSize(new Dimension(ObjetoSize.EXP.getWidth(), ObjetoSize.EXP.getHeight()));
-        add(right, BorderLayout.EAST);
-        right.setBackground(Color.lightGray);
-
-        // Agregar subpaneles al panel principal
-        add(left, BorderLayout.CENTER);
-        add(right, BorderLayout.EAST);
+        exp = new PanelExpendedor(0, 0, modeloExpendedor);
+        com = new PanelComprador(0, 0, modeloComprador);
+        exp.setPreferredSize(new Dimension(ObjetoSize.EXP.getWidth(), ObjetoSize.EXP.getHeight()));
+        exp.addMouseListener(this);
+        com.setPreferredSize(new Dimension(ObjetoSize.COM.getWidth(), ObjetoSize.COM.getHeight()));
+        add(exp, BorderLayout.EAST);
+        add(com, BorderLayout.CENTER);
 
         // Expendedor al panel de la derecha
-        exp = new PanelExpendedor(0, 0, modeloExpendedor);
-        right.add(exp, BorderLayout.CENTER);
 
         // -------------MENÚ----------------
         // Crear los paneles
@@ -145,13 +145,11 @@ public class PanelPrincipal extends JPanel implements ActionListener {    // Se 
             BigPanel.remove(panel2);
             BigPanel.add(panel1);
             SwingUtilities.updateComponentTreeUI(this);
-        }
-        else if (e.getSource() == boton2) {
+        } else if (e.getSource() == boton2) {
             BigPanel.remove(panel1);
             BigPanel.add(panel3);
             SwingUtilities.updateComponentTreeUI(this);
-        }
-        else if (e.getSource() == boton11) {
+        } else if (e.getSource() == boton11) {
             BigPanel.remove(panel3);
             BigPanel.add(panel1);
             SwingUtilities.updateComponentTreeUI(this);
@@ -161,20 +159,16 @@ public class PanelPrincipal extends JPanel implements ActionListener {    // Se 
         else if (e.getSource() == boton4) {
             cualProducto = ProductoTipo.SUPER8.getNumero();
             JOptionPane.showMessageDialog(null, "Bebida " + ProductoTipo.SUPER8.toString().toLowerCase() + " seleccionada");
-        }
-        else if (e.getSource() == boton5) {
+        } else if (e.getSource() == boton5) {
             cualProducto = ProductoTipo.SNICKERS.getNumero();
             JOptionPane.showMessageDialog(null, "Bebida " + ProductoTipo.SNICKERS.toString().toLowerCase() + " seleccionada");
-        }
-        else if (e.getSource() == boton12) {
+        } else if (e.getSource() == boton12) {
             cualProducto = ProductoTipo.COCA.getNumero();
             JOptionPane.showMessageDialog(null, "Bebida " + ProductoTipo.COCA.toString().toLowerCase() + " seleccionada");
-        }
-        else if (e.getSource() == boton13) {
+        } else if (e.getSource() == boton13) {
             cualProducto = ProductoTipo.SPRITE.getNumero();
             JOptionPane.showMessageDialog(null, "Bebida " + ProductoTipo.SPRITE.toString().toLowerCase() + " seleccionada");
-        }
-        else if (e.getSource() == boton14) {
+        } else if (e.getSource() == boton14) {
             cualProducto = ProductoTipo.FANTA.getNumero();
             JOptionPane.showMessageDialog(null, "Bebida " + ProductoTipo.FANTA.toString().toLowerCase() + " seleccionada");
         }
@@ -183,16 +177,13 @@ public class PanelPrincipal extends JPanel implements ActionListener {    // Se 
         else if (e.getSource() == boton7) {
             modeloMoneda = new Moneda100();
             JOptionPane.showMessageDialog(null, "Moneda de $100 seleccionada");
-        }
-        else if (e.getSource() == boton8) {
+        } else if (e.getSource() == boton8) {
             modeloMoneda = new Moneda500();
             JOptionPane.showMessageDialog(null, "Moneda de $500 seleccionada");
-        }
-        else if (e.getSource() == boton9) {
+        } else if (e.getSource() == boton9) {
             modeloMoneda = new Moneda1000();
             JOptionPane.showMessageDialog(null, "Moneda de $1000 seleccionada");
-        }
-        else if (e.getSource() == boton10) {
+        } else if (e.getSource() == boton10) {
             modeloMoneda = new Moneda1500();
             JOptionPane.showMessageDialog(null, "Moneda de $1500 seleccionada");
         }
@@ -220,7 +211,36 @@ public class PanelPrincipal extends JPanel implements ActionListener {    // Se 
     public void paintComponent (Graphics g) {   // Todo se dibuja a partir de este método
         super.paintComponent(g);                // Llama al método paint al que hace override en la super clase
                                                 // El de la super clase solo pinta el fondo (background)
-        //com.paintComponent(g);                // Llama al metodo paintComponent definido en el PanelComprador
+        com.paintComponent(g);                  // Llama al metodo paintComponent definido en el PanelComprador
         exp.paintComponent(g);                  // Llama al metodo paintComponent definido en el PanelExpendedor
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        if (e.getSource() == exp) {             // Si se cliquea el expendedor
+            System.out.println("Expendedor rellenado");
+            modeloExpendedor.rellenarProductos(numProductos);
+            repaint();
+        }
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
     }
 }
