26e616f6c1f561bbbcfca155eee721848525a573
byNoRx <norxgamer12@gmail.com>
2025-06-09 10:36:29 -0400

    Recrear ventana para que se ajuste automáticamente al panel principal

--- src/vista/Ventana.java
+++ src/vista/Ventana.java
@@ -2,19 +2,20 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Ventana extends JFrame {
-    private PanelPrincipal panelPrincipal; // Un panel para objetos gráficos
+    private PanelPrincipal panelPrincipal;
 
-    Ventana() {
-        super(); // Hereda metodos de JFrame
+    public Ventana() {
+        personalizar();
 
         panelPrincipal = new PanelPrincipal();
-        panelPrincipal.setLayout(new BorderLayout());
-        Menu menu = new Menu();
-        panelPrincipal.add(menu,BorderLayout.EAST);
+        add(panelPrincipal, BorderLayout.CENTER);
+        pack();
+    }
 
-        this.add(panelPrincipal,BorderLayout.CENTER); // Se agrega al centro
-        this.setDefaultCloseOperation(EXIT_ON_CLOSE); // Cerrar aplicación
-        this.setSize(600,800);
-        this.setVisible(true);
-    } 
+    private void personalizar() {
+        setTitle("Expendedor y Comprador");
+        setLayout(new BorderLayout());
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
 }
